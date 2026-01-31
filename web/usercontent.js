c6029fc78eb47f45812fd5be2bcb74a84e981fe0
shc0743 <shc0743@outlook.com>
2026-02-01 04:39:17 +0800

    fix(usercontent): fix the wrong script path

--- web/usercontent.js
+++ web/usercontent.js
@@ -1,6 +1,6 @@
 ((async function () {
-    const mainfile = await (await fetch('/__/sandbox/runtime/mainfile.txt', { cache: 'no-cache' })).text();
+    const manifest = await (await fetch('/__/sandbox/runtime/.vite/manifest.json', { cache: 'no-cache' })).json();
     const script = document.createElement('script');
-    script.src = `/__/sandbox/runtime/${mainfile}`;
+    script.src = `/__/sandbox/runtime/${manifest['src/ucs-runtime.ts'].file}`;
     document.head.append(script);
 })());
