cdf268adf130b345f4147d84ed3c8f0cd53a1a0b
shc0743 <shc0743@outlook.com>
2026-02-25 22:45:11 +0800

    fix(web): fix wrong config check

--- web/index.js
+++ web/index.js
@@ -1,16 +1,15 @@
 ((async function () {
     const { HOST_BASE, USERCODE_PLATFORM } = await (await fetch('/__/config.json')).json();
     const { hostname, pathname } = window.location;
-    if (!hostname.endsWith(HOST_BASE)) {
+    if (!hostname.endsWith(HOST_BASE) && !hostname.endsWith(PLATFORM_BASE)) {
         window.document.body.appendChild(window.document.createElement('h1')).innerHTML = "Error: Invalid Hostname";
         return;
     }
     const isEmbedded = (window.parent !== window.self);
-    const host_prefix = hostname.slice(0, hostname.length - HOST_BASE.length);
+    const host_prefix = hostname.slice(0, hostname.length - (hostname.endsWith(HOST_BASE) ? HOST_BASE : PLATFORM_BASE).length);
     if (!host_prefix || host_prefix === 'www.') {
         if (isEmbedded) {
             // // Randomly pick a subdomain for embedding
-            // const random_subdomain = window.crypto.randomUUID().replace(/-/g, '');
             const r = (Math.random() * 1e10) % 10; // 0-9
             window.location.href = `https://public${r}.${HOST_BASE}/usercontent.html#${pathname}`;
         }
